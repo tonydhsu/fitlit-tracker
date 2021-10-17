54a327f85b797424e80eba6378cee411dd9127a9
Tanya Zhuge <tangnazhuge@gmail.com>
2021-10-17 11:13:56 -0700

    Add User class and User test

--- test/User-test.js
+++ test/User-test.js
@@ -2,7 +2,57 @@ const expect = require('chai').expect;
 const User = require('../src/User.js');
 
 describe('User', () => {
+  let user;
+  beforeEach(function() {
+    user = new User({
+      id: 1,
+      name: 'Brian Peterson',
+      address: '1 North Pole',
+      email: 'brian@northpole.com',
+      strideLength: 5,
+      dailyStepGoal: 11,
+      friends: [4, 9, 11],
+    })
+  })
+
   it('should be a function', function () {
     expect(User).to.be.a('function');
   });
+
+  it('should be an instance of User', function () {
+    expect(user).to.be.an.instanceOf(User);
+  });
+
+  it('should store an id', function() {
+    expect(user.id).to.equal(1);
+  });
+
+  it('should store a name', function() {
+    expect(user.name).to.equal('Brian Peterson');
+  });
+
+  it('should store an address', function() {
+    expect(user.address).to.equal('1 North Pole');
+  });
+
+  it('should store an email', function() {
+    expect(user.email).to.equal('brian@northpole.com');
+  });
+
+  it('should store a strideLength', function() {
+    expect(user.strideLength).to.equal(5);
+  });
+
+  it('should store a daily step goal', function() {
+    expect(user.dailyStepGoal).to.equal(11);
+  });
+
+  it('should store an array of friends', function() {
+    expect(user.friends).to.deep.equal([4, 9, 11]);
+  });
+
+  it('should return first name of user', function() {
+    expect(user.returnFirstName()).to.equal('Brian');
+  });
+
 });
