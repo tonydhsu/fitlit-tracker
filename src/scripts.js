caecf4952a1715c47d572d3ff47bde8907a9bb4b
Brian Peterson <bpeterson1579@gmail.com>
2021-10-18 14:58:01 -0600

    Attach script file to HTML through webpack

--- src/scripts.js
+++ src/scripts.js
@@ -7,9 +7,39 @@ import './css/styles.css';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-console.log('This is the JavaScript entry file - your code begins here.');
-
 // An example of how you tell webpack to use a JS file
 
 import userData from './data/users';
 import UserRepository from './UserRepository';
+import User from './User';
+
+let users = userData;
+
+const infoCard = document.getElementById('cardInfo');
+const stepComparison = document.getElementById('stepComparison');
+
+let user = new User(userData[0]);
+
+const onPageLoad = () => {
+  infoCard.innerHTML += `
+  <h2>Hi ${user.returnFirstName()}!</h2>
+  <h3>User Information</h3>
+  <h4>Name:</h4>
+  <p>${user.name}</p>
+  <h4>Email:</h4>
+  <p>${user.email}</p>
+  <h4>Address:</h4>
+  <p>${user.address}</p>
+  <h4>Stride Length</h4>
+  <p>${user.strideLength}</p>
+  <h4>Daily Step Goal</h4>
+  <p>${user.dailyStepGoal}</p>`
+  compareSteps();
+}
+
+const compareSteps = () => {
+  let users = new UserRepository(userData);
+  stepComparison.innerText = `The average step goal amoungst all users is: ${users.retrieveUsersAvgStepGoals()}. Your step goal is ${user.dailyStepGoal}.`;
+}
+
+window.addEventListener('load', onPageLoad);
