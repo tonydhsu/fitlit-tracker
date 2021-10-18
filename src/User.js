78b777a1c2873b3ef3d5d70f96adc21dc10d83db
Brian Peterson <bpeterson1579@gmail.com>
2021-10-18 14:57:26 -0600

    Fix formatting

--- src/User.js
+++ src/User.js
@@ -1,17 +1,17 @@
 const data = require('../src/data/users.js')
 
 class User {
-  constructor(userData)
-  {this.id = userData.id;
-  this.name = userData.name;
-  this.address = userData.address;
-  this.email = userData.email;
-  this.strideLength = userData.strideLength;
-  this.dailyStepGoal = userData.dailyStepGoal;
-  this.friends = userData.friends;
+  constructor(userData) {
+    this.id = userData.id;
+    this.name = userData.name;
+    this.address = userData.address;
+    this.email = userData.email;
+    this.strideLength = userData.strideLength;
+    this.dailyStepGoal = userData.dailyStepGoal;
+    this.friends = userData.friends;
   }
 
-  returnFirstName (){
+  returnFirstName () {
     return this.name.split(' ')[0]
   }
 }
