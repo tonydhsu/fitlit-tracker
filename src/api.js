f159687ffb05acd6cce5ed3a4d2bef70add25cc0
Carly Collums <carlycollums@gmail.com>
2021-11-02 15:27:08 -0600

    Add new local server api

--- src/api.js
+++ src/api.js
@@ -1,28 +1,44 @@
 // import {renderInfoCard, renderWaterInfo, renderActivityInfo, renderSleepInfo, createInitialDashboard} from "./scripts";
 
 const getUserData = () => {
-  return fetch("https://pacific-badlands-43237.herokuapp.com/api/v1/users")
+  return fetch("http://localhost:3001/api/v1/users")
     .then(response => response.json())
     .then((data) => {return data})
 };
 
 const getSleepData = () => {
-  return fetch("https://pacific-badlands-43237.herokuapp.com/api/v1/sleep")
+  return fetch("http://localhost:3001/api/v1/sleep")
     .then(response => response.json())
     .then((data) => {return data})
 };
 
 const getActivityData = () => {
-  return fetch("https://pacific-badlands-43237.herokuapp.com/api/v1/activity")
+  return fetch("http://localhost:3001/api/v1/activity")
     .then(response => response.json())
     .then((data) => {return data})
 };
 
 const getHydrationData = () => {
-  return fetch("https://pacific-badlands-43237.herokuapp.com/api/v1/hydration")
+  return fetch("http://localhost:3001/api/v1/hydration")
     .then(response => response.json())
     .then((data) => {return data})
 };
+
+
+
+
+// const fetchData = (param) => {
+//     return fetch(`https://pacific-badlands-43237.herokuapp.com/api/v1/${param}`)
+//       .then(response => response.json())
+//       .then((data) => {return data})
+// }
+
+// const allData = () => {
+//   Promise.all([fetchData(users), fetchData(sleep), fetchData(activity), fetchData(hydration)])
+//     .then(values => {return values})
+// };
+//
+// console.log(allData())
 
 export {
   getUserData,
